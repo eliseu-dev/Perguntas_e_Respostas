585057b27ede768e4f2bb7a96c028ff02f9802cc
eliseugomes <eliseu.uepb@gmail.com>
2021-07-01 09:52:37 -0300

    adicionando bootstrap

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.get("/",(req, res) =>{
+    res.render("index")
+})
+app.get("/asking", (req,res) =>{
+    res.render('asking')
+})
+app.post("/savequestions", (req,res) =>{
+    const title = req.body.title
+    const description = req.body.description
+    res.send(`Formulário recebido!
+    Titulo: ${title}
+    Descrição: ${description}`)
+})
+
+
+app.listen(3000, () => {console.log("server online")})
